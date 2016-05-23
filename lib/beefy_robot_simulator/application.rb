943ae7349b2ceaaabe4b3060bbe4fec80e11431e
damonjmurray <damonjmurray@gmail.com>
2016-05-23 23:17:58 +0800

    Stub public interface methods on Application
    - add method stubs for #place, #left, #right, #move and #report

--- lib/beefy_robot_simulator/application.rb
+++ lib/beefy_robot_simulator/application.rb
@@ -5,4 +5,30 @@ class Application
     @robot = Robot.new
     @table = Table.new
   end
+
+  def place
+    # TODO: place robot on board and set its direction
+    raise NotImplementedError
+  end
+
+  def left
+    # TODO: change robot's direction 90 degrees to the left
+    raise NotImplementedError
+  end
+
+  def right
+    # TODO: change robot's direction 90 degrees to the right
+    raise NotImplementedError
+  end
+
+  def move
+    # TODO: change position of robot on board by one unit in the
+    #       direction the robot is currently facing
+    raise NotImplementedError
+  end
+
+  def report
+    # TODO: report the current position and direction of the robot
+    raise NotImplementedError
+  end
 end
